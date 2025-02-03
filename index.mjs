a2a32cbb9baec71d8b5ffc6e1f3a63923d2c0c40
Kiril <fanky.yahoo@gmail.com>
2025-02-03 11:33:41 +0200

    ADD: synchronous file system using

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import logger from './logger.mjs';
 import fs from 'node:fs'
-// const data = fs.readFileSync('./index.mjs');
-// logger.info(data);
-fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'))
+const data = fs.readFileSync('./index.mjs','utf8');
+console.log(data)
+fs.writeFileSync('./file.txt', ["kuku", "kukureku", "Hello World!"].join('\n'), 'utf8')
