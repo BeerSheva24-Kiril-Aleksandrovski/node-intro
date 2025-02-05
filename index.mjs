ebfbbbeb0493a2a83e600f109fc46054cc511437
Kiril <fanky.yahoo@gmail.com>
2025-02-05 14:07:04 +0200

    ADD: task for Logger with EventEmitter functionality

--- index.mjs
+++ index.mjs
@@ -1,6 +1,4 @@
+
 import logger from './logger.mjs';
-import { readFile, writeFile } from 'node:fs/promises'
-const data = await readFile('./file.txt', 'utf-16le')
-console.log(data)
-writeFile('./file.txt', ["kuku", "kukureku", "שלום"].join('\n'), 'utf-16le')
-logger.info("function finished");
+//TODO
+//main controller functionality
