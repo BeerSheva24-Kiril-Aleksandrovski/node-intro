3138b2f6e050ca1d6464727f952426bbe4ed1c8e
Kiril <fanky.yahoo@gmail.com>
2025-02-09 10:20:50 +0200

    ADD: http server introduction

--- index.mjs
+++ index.mjs
@@ -2,7 +2,11 @@ import http from 'node:http'
 const server = http.createServer();
 const port = process.env.PORT ?? 3500;
 server.listen(port);
-console.log (`listening on the port ${port} `)
+console.log(`listening on the port ${port} `)
 server.on('request', async (req, res) => {
-    console.log(req.method, req.url)
+    console.log(req.method, req.url);
+    res.setHeader("Content-Type", "application/json");
+    res.statusCode = 300;
+    res.write("just bad request");
+    res.end();
 })
