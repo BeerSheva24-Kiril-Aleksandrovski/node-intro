ca3fad5d5b4e6b7ad63e246476b5935f8183aeb2
Kiril <fanky.yahoo@gmail.com>
2025-02-10 10:20:54 +0200

    FIX: removed unused stuff, comments added

--- src/protocol/protocol.mjs
+++ src/protocol/protocol.mjs
@@ -1,25 +1,28 @@
-const protocolObj= {
-       "/addPoint": addPoint,
-       "/getPoint": getPoint
+/*
+here should be the code for actual protocol
+*/
+const protocolObj = {
+    "/addPoint": addPoint,
+    "/getPoint": getPoint
 }
 function createResponse(code, response) {
-    return {code, response};
+    return { code, response };
 }
-function addPoint (data, server, service) {
-        
+function addPoint(data, server, service) {
+
     try {
         service.addPoint(JSON.parse(data));
         server.emit("response", createResponse(204, ""));
     } catch (error) {
         server.emit("response", createResponse(400, error.message))
     }
-   }
-   function getPoint (data, server, service) {
-        try {
-            const point = service.getPoint(data);
-            server.emit("response", createResponse(200,JSON.stringify(point)));
-        } catch(error) {
-            server.emit("response", createResponse(404, error.message))
-        }
-   }
-    export default protocolObj;
+}
+function getPoint(data, server, service) {
+    try {
+        const point = service.getPoint(data);
+        server.emit("response", createResponse(200, JSON.stringify(point)));
+    } catch (error) {
+        server.emit("response", createResponse(404, error.message))
+    }
+}
+export default protocolObj;
